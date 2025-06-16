35d007ca074f879491d154ff97e7712ac28fcb66
jahnavi mantripragada <43176862+Jahnavi-Mantripragada@users.noreply.github.com>
2025-06-16 18:24:15 +0530

    Refine layout with grid and extend theme tokens

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 import "./styles/styles.css";
+import theme from "./themeStore.js";
+
+Object.entries(theme.spacing).forEach(([key, value]) => {
+  document.documentElement.style.setProperty(`--space-${key}`, value);
+});
+Object.entries(theme.sizing).forEach(([key, value]) => {
+  document.documentElement.style.setProperty(`--size-${key}`, value);
+});
+
 import "./app.js";
